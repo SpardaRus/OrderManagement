24bce0a2011ecba8e0b5eb386d9a85493031685f
SpardaRus <ale-altair@yandex.ru>
2020-05-14 03:31:50 +0400

    Fix validate request

--- src/main/java/com/spardarus/controller/auth/request/LoginRequest.java
+++ src/main/java/com/spardarus/controller/auth/request/LoginRequest.java
@@ -2,25 +2,19 @@ package com.spardarus.controller.auth.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
-import java.util.regex.Pattern;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class LoginRequest {
 
     @Email(message = "Incorrect email")
+    @NotNull(message = "Please fill email")
     private String email;
+
+    @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@*#$%]).{8,20})",
+            message = "Incorrect password")
+    @NotNull(message = "Please fill password")
     private String password;
-
-    @AssertTrue(message = "Incorrect password")
-    public boolean passwordIsValid() {
-        String PASSWORD_PATTERN_REGEXP =
-                "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@*#$%]).{8,20})";
-        Pattern passwordPattern = Pattern.compile(PASSWORD_PATTERN_REGEXP);
-//        return passwordPattern.matcher(password).matches();
-        return false;
-    }
-
-
 }
